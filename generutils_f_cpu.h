2e29a244177cdd151971d61090d9d8bfcf14273c
huynq-apm <101412193+huynq-apm@users.noreply.github.com>
2022-03-11 22:06:39 +0700

    Update generutils_f_cpu.h

--- generutils_f_cpu.h
+++ generutils_f_cpu.h
@@ -6,9 +6,7 @@
  *
  * F_CPU value is required for correct operation of the some utilities. 
  * Those utilities are setup to use the F_CPU value defined here
- *
- * The prototype for generutil_f_cpu.h is _generutil_f_cpu.h
- * Copy the prototype to your project, and remove '_'
+ * Clone and modify this file
  */
 
 /* Define F_CPU below this line with #define or #include */
